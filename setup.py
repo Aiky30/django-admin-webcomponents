e1821c394f9039970a262d89055a7d70a3ba2905
Andrew Aikman <andrew.aikman@eliga.services>
2021-10-07 18:29:09 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="PackageName",
+    version="0.1.0",
+    author="",
+    author_email="",
+    packages=[],
+    scripts=[],
+    url="",
+    license="LICENSE.txt",
+    description="",
+    long_description=open("README.rst").read(),
+    install_requires=[],
+)
